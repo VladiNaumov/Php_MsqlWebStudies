9f5d5adefe612e80e74291f2b5a46df130c74933
Sim <vov4.games@yandex.ru>
2021-10-01 18:02:43 +0300

    Php

---
+++ config/config_db.php
@@ -0,0 +1,9 @@
+<?php
+
+//настройки соеденения с сервером БД
+
+    return[
+        'dsn' => 'mysql:host=localhost;dbname=u1322686_demo;charset=utf8',
+        'user' => 'u1322686_admin',
+        'pass' => 'sI3eJ2jV3bhA7j',
+        ];
---
+++ public/index.php
@@ -0,0 +1,41 @@
+<?php
+
+error_reporting(-1);
+
+// считывания данных с URL браузера
+$query = rtrim($_SERVER['QUERY_STRING'], '/');
+
+require '../libs/myDebug.php';
+
+const TOKEN = '2006101055:AAEE98ckdoAzDCJ5bZBVyV9txN3b-s3HfIQ';
+
+const BASE_ULR = 'https://api.telegram.org/bot' . TOKEN . '/';
+
+$url = BASE_ULR . 'getUpdates';
+
+
+/* json_decode - Декодирует строку JSON
+
+ * Если true, объекты JSON будут возвращены как ассоциативные массивы (array); если false, объекты JSON будут возвращены как объекты (object).
+ * Если null, объекты JSON будут возвращены как ассоциативные массивы (array) или объекты (object) в зависимости от того, установлена ли JSON_OBJECT_AS_ARRAY в flags.
+ *
+ * file_get_contents - Читает содержимое файла в строку
+ */
+$res = json_decode(file_get_contents($url), false);
+
+debug($res);
+
+
+
+
+/*
+ * Дваеточие - это обращение к статическому методу
+ * Ruoter::dispatch($query);
+ *
+ * $DEMO = new DemoDemo();
+ * $DEMO->myDemo();
+ * $DEMO->myDemo();
+ *
+ * Дваеточие - это обращение к статическому методу
+ * $DEMO::myStaticDemo();
+ */
