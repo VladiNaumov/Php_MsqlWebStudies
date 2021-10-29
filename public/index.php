210ba1b58acddff2304ef26334e134b1d13211e5
Sim <vov4.games@yandex.ru>
2021-10-29 13:28:55 +0300

    Php

--- public/index.php
+++ public/index.php
@@ -1,15 +1,15 @@
 <?php
 
 
-
 // phpinfo();
 
 error_reporting(-1);
 
-echo 'HELLO GAYs';
+echo '<pre> HELLO GAYs </pre>';
 
 require '../libs/myDebug.php';
 require 'TelegramBot.php';
+
 
 // Telegram token
 const TOKEN = '';
@@ -20,44 +20,66 @@ const BASE_ULR = 'https://api.telegram.org/bot' . TOKEN . '/';
 
 const START = false;
 
+
+$boot = new TelegramBot(TOKEN);
+
+$bootUpdate = $boot->sendRequest('getUpdates');
+
+echo ' $boot->sendRequest(getUpdates); ';
+debug($bootUpdate);
+
+
+$bootUpdate = $boot->getUpdates();
+
+echo '$boot->getUpdates();';
+debug($bootUpdate);
+
 while (START) {
 
+
+    //
+    $bootUpdate = $boot->getUpdates();
+
+    if (!empty($updates)) {
+
+        //
+         file_put_contents(__DIR__ . '/logs.txt', print_r($updates, 1), FILE_APPEND);
+
+        foreach ($updates as $bootUpdate) {
+
+            echo $bootUpdate->message->text . PHP_EOL;
+
+            $boot->sendMessage($bootUpdate->message->chat->id, "Привет, {$bootUpdate->message->from->first_name}! Вы написали: {$bootUpdate->message->text}");
+        }
+    }
+
+    sleep(3);
 
 }
 
 
 /*
- *Method getUpdate - Этот метод используется для получения обновлений через long polling (wiki).
+ * getUpdate() - Этот метод используется для получения обновлений через long polling (wiki).
  * Ответ возвращается в виде массива объектов Update.
 */
 
 
 /*
- * sendMessage
+ * sendMessage()
  * Use this method to send text messages. On success, the sent Message is returned
  */
 
 
 
 /*
- * json_decode - Декодирует полученную JSON
+ * json_decode() - Декодирует полученную JSON
  * Если true, объекты JSON будут возвращены как ассоциативные массивы (array); если false, объекты JSON будут возвращены как объекты (object).
  * Если null, объекты JSON будут возвращены как ассоциативные массивы (array) или объекты (object) в зависимости от того, установлена ли JSON_OBJECT_AS_ARRAY в flags.
  *
- * file_get_contents - Читает содержимое файла в строку
+ * file_get_contents() - Читает содержимое файла в строку
  */
 
 
-
-/* INFO */
-
-/*
- *json_decode - Декодирует строку JSON
- * Если true, объекты JSON будут возвращены как ассоциативные массивы (array); если false, объекты JSON будут возвращены как объекты (object).
- * Если null, объекты JSON будут возвращены как ассоциативные массивы (array) или объекты (object) в зависимости от того, установлена ли JSON_OBJECT_AS_ARRAY в flags.
- *
- * file_get_contents - Читает содержимое файла в строку
- */
 
 
 
